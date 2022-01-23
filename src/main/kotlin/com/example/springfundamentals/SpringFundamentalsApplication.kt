21c3db20c07d6f2f0b0fa3e6e18e94c500cd5d48
yong <yonggeon.kim@gmail.com>
2022-01-24 01:09:07 +0900

    Diable spring boot

--- src/main/kotlin/com/example/springfundamentals/SpringFundamentalsApplication.kt
+++ src/main/kotlin/com/example/springfundamentals/SpringFundamentalsApplication.kt
@@ -1,11 +1,11 @@
 package com.example.springfundamentals
-
-import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.runApplication
-
-@SpringBootApplication
-class SpringFundamentalsApplication
-
-fun main(args: Array<String>) {
-    runApplication<SpringFundamentalsApplication>(*args)
-}
+//
+//import org.springframework.boot.autoconfigure.SpringBootApplication
+//import org.springframework.boot.runApplication
+//
+//@SpringBootApplication
+//class SpringFundamentalsApplication
+//
+//fun main(args: Array<String>) {
+//    runApplication<SpringFundamentalsApplication>(*args)
+//}
