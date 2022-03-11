33268ef32e33fa85d64defb75df59bfddcbba948
drevispas <yonggeon.kim@gmail.com>
2022-03-11 14:21:07 +0900

    Chapter 2.1.2 to 2.1.4

--- src/main/kotlin/com/example/springfundamentals/Beans.kt
+++ src/main/kotlin/com/example/springfundamentals/Beans.kt
@@ -1,14 +1,11 @@
 package com.example.springfundamentals
 
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.beans.factory.annotation.Qualifier
-import org.springframework.context.annotation.*
-import org.springframework.stereotype.Component
-import javax.annotation.PostConstruct
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 
 class Car(val name: String = "car") {
     init {
-        println("Car $name is created")
+        println("Car `$name` is created.")
     }
 }
 
@@ -16,50 +13,8 @@ class Car(val name: String = "car") {
 class BeanConfig {
     @Bean
     fun car2(): Car {
-        return Car("car2")
+        return Car("car2") // 스프링이 반환객체를 context에 추가
     }
     @Bean
     fun long() = 1L
-//    @Bean
-//    fun hello() = "hello!"
-    @Bean
-    @Primary
-    fun car1() = Car("car1")
-    @Bean
-    fun carOwner1() = CarOwner("carOwner1", car2())
-    @Bean
-    fun carOwner2(car: Car) = CarOwner("carOwner2", car)
 }
-
-@Component
-data class Bike(val name: String = "bike") {
-    init {
-        println("something pre")
-    }
-
-    @PostConstruct
-    fun post() = println("something post")
-}
-
-@Configuration
-@ComponentScan(basePackages = ["com.example.springfundamentals"])
-class ComponentConfig
-
-@Configuration
-class RegisterBeanConfig
-
-class CarOwner(val name: String = "carOwner", val car: Car)
-
-@Component
-class BikeOwner {
-    val name: String = "bikeOwner"
-//    @Autowired
-//    lateinit var bike: Bike
-    val bike: Bike
-    constructor(bike: Bike){
-        this.bike = bike
-    }
-}
-
-@Component
-class CarOwnerWithQualified(val name: String = "carOwnerWithQualified", @Qualifier("car2") val car: Car)
